3bdc12a0caa409feafbb3000e1f74fca66e9bc82
ZeroxBlack <zeroxblack.fw@gmail.com>
2020-07-10 03:01:18 -0400

    First Commit

---
+++ src/index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const { dirname } = require('path');
+
+// Settings
+app.set('port', 3000);
+app.set('views', path.join(__dirname, 'views'));
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'ejs');
+
+// Middlewares
+
+// Routes
+
+app.use(require('./routes/index'));
+
+
+/*
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/index.html'));
+})
+*/
+
+// Static Files
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Listening the server
+app.listen(app.get('port'), () => {
+    console.log('Server on port', app.get('port'));
+})
---
+++ src/routes/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res) => {
+    res.render('index.html', { title: 'Home Page' });
+})
+
+router.get('/about', (req, res) => {
+    res.render('about.html', { title: 'About Page' });
+})
+
+router.get('/contact', (req, res) => {
+    res.render('contact.html', { title: 'Contact Page' });
+})
+
+
+module.exports = router;
